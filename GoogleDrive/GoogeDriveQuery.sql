ebe0a7209660fee948b568fbe1158906307f7a27
killiannn <62028579+killiannn@users.noreply.github.com>
2025-07-24 17:59:52 +0700

    query

--- GoogleDrive/GoogeDriveQuery.sql
+++ GoogleDrive/GoogeDriveQuery.sql
@@ -1,6 +1,17 @@
 USE GoogleDrive;
 GO
 
+--Home
+--get user information
+select 
+	u.Name as UserName,
+	u.Email as UserEmail,
+	u.LastLogin as UserLastLogin
+from [User] u
+where u.id = 1;
+
+
+--My Drive
 --get my folder
 select
 	f.Name as FolderName,
@@ -35,15 +46,7 @@ join [User] u on su.UserId = u.Id
 join Setting s on su.SettingId = s.Id
 where u.Id =1
 
-
---get user information
-select 
-	u.Name as UserName,
-	u.Email as UserEmail,
-	u.LastLogin as UserLastLogin
-from [User] u
-where u.id = 1;
-
+--Starred
 --get starred
 SELECT 
     fo.ObjectId,
@@ -61,17 +64,41 @@ LEFT JOIN [File] fi ON fo.ObjectId = fi.Id AND fo.ObjectTypeId = 2
 WHERE fo.OwnerId = 1
 ORDER BY ot.Name;
 
+--get recommend folder/file
+select
+	r.Id,
+	case
+		when r.ObjectTypeId = 1 then f.Name
+		else fi.Name
+	end as [Name],
+	r.Log,
+	r.DateTime
+from Recent r
+left join Folder f on f.Id = r.ObjectId and r.ObjectTypeId = 1
+left join [File] fi on fi.Id = r.ObjectId and r.ObjectTypeId = 2
+join [User] u on r.UserId = u.Id
+where u.Id = 1
+order by r.DateTime DESC;
+
+
 --get recent
 SELECT 
-    r.FileId,
-    f.Name AS FileName,
-    f.Path AS FilePath,
+    r.Id,
+    case
+		when r.ObjectTypeId = 1 then f.Name
+		else fi.Name
+	end as [Name],
+	case
+		when r.ObjectTypeId = 1 then f.Path
+		else fi.Path
+	end as [Name],
     ft.Name AS FileType,
     r.DateTime AS LastAccessed
 FROM Recent r
-JOIN [User] u ON r.UserId = u.Id
-JOIN [File] f ON r.FileId = f.Id
-JOIN FileType ft ON f.FileTypeId = ft.Id
+JOIN [User] u ON r.Id = u.Id
+LEFT JOIN Folder f ON f.Id = r.ObjectId and r.ObjectTypeId = 1
+LEFT JOIN [File] fi ON fi.Id = r.ObjectId and r.ObjectTypeId =2
+JOIN FileType ft ON fi.FileTypeId = ft.Id
 WHERE r.UserId = 1
 ORDER BY r.DateTime DESC;
 
@@ -90,7 +117,7 @@ SELECT
     s.ExpiresAt AS ShareExpires
 FROM SharedUser su
 JOIN Share s ON su.ShareId = s.Id
-JOIN [User] u ON su.UserId = u.Id
+JOIN [User] u ON su.Id = u.Id
 JOIN [User] u2 ON s.Sharer = u2.Id
 JOIN ObjectType ot ON s.ObjectTypeId = ot.Id
 JOIN Permission p ON su.PermissionId = p.Id
@@ -125,3 +152,11 @@ select
 from [User] u
 where u.Id = 1
 order by RemainingCapacity DESC;
+
+--get list file by size
+select
+	f.Name as FileName,
+	f.Size
+from [File] f
+join [User] u on f.OwnerId = u.Id
+order by f.Size DESC;
